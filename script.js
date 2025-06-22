fa63d4d7acd5c6fe54361aee8687ac7765518f39
Yehia <ydawoud777@gmail.com>
2025-06-23 01:13:12 +0400

    User can now change grid settings (width, height, color)

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function onSquareHover(e) {
+function onSquareHover(e, color) {
   e.target.style.backgroundColor = color;
   console.log("hi");
 }
-
 function initGrid(width, height, color) {
   const screenWidth = document.documentElement.clientWidth;
-  const screenHeight = document.documentElement.clientHeight;
+  const screenHeight = document.documentElement.clientHeight - 20;
 
-  const squareHeight = Math.floor(screenHeight/height);
-  const squareWidth = Math.floor(screenWidth/width);
+  const squareHeight = screenHeight/height;
+  const squareWidth = screenWidth/width;
 
   const gridWidth = Math.min(squareHeight, squareWidth) * width;
   const gridHeight = Math.min(squareHeight, squareWidth) * height;
 
   const grid = document.querySelector(".grid");
+  grid.innerHTML = "";
   grid.style.height = `${gridHeight}px`;
   grid.style.width = `${gridWidth}px`;
 
@@ -24,7 +24,8 @@ function initGrid(width, height, color) {
     for (let j = 0; j < width; j++) {
       const square = document.createElement("div");
       square.classList.add("grid-square");
-      square.addEventListener("mouseover", e=>onSquareHover(e, color));
+      console.log(color)
+      square.addEventListener("mouseenter", e=>onSquareHover(e, color));
 
       row.appendChild(square);
     }
@@ -32,6 +33,14 @@ function initGrid(width, height, color) {
     grid.appendChild(row);
   }
 }
+function changeSettings() {
+  const width = parseInt(prompt("width", 16));
+  const height = parseInt(prompt("height", 16));
+  const color = prompt("color", "black");
+
+  initGrid(width, height, color);
+}
+document.querySelector("button.change-settings").addEventListener("click", changeSettings);
 
 const width = 16, height = 16;
 const color = "green";
